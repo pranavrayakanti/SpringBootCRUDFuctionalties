e1e9827ce1fdd1aaafffabcf09209262b23766d1
Pranav <Pranav@10.0.0.30>
2019-01-03 23:25:35 -0500

    test

--- src/main/java/com/pranav/topics/TopicRepo.java
+++ src/main/java/com/pranav/topics/TopicRepo.java
@@ -3,5 +3,8 @@ package com.pranav.topics;
 import org.springframework.data.repository.CrudRepository;
 
 public interface TopicRepo extends CrudRepository<Topics, Integer> {
+	
+	
+	//repo layer
 
 }
